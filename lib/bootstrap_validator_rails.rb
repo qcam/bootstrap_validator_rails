ca4e1e05a52f19f623481af2c4ee1acc6f93d24b
Jack A. Huang <huynhquancam@gmail.com>
2014-07-25 20:32:10 +0700

    Remove unused file

--- lib/bootstrap_validator_rails.rb
+++ lib/bootstrap_validator_rails.rb
@@ -1,7 +1,6 @@
 require 'bootstrap_validator_rails/engine'
 require 'bootstrap_validator_rails/form_builder'
 require 'bootstrap_validator_rails/helper'
-require 'bootstrap_validator_rails/validator_message'
 require 'bootstrap_validator_rails/validators/attributes'
 require 'bootstrap_validator_rails/validators/generator'
 require 'bootstrap_validator_rails/validators/presence_validator'
