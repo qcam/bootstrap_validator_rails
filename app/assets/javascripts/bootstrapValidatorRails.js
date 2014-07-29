ce2b4c35bbfe394c30d106be87d9eb9d6d9d87a8
Jack A. Huang <huynhquancam@gmail.com>
2014-07-29 16:04:16 +0700

    Add bootstrapValidatorRails script

---
+++ app/assets/javascripts/bootstrapValidatorRails.js
@@ -0,0 +1,9 @@
+/***
+ * BootstrapValidatorRails
+ *
+ * @author Jack A. Huang
+ * @url http://github.com/huynhquancam/bootstrap_validator_rails/
+ */
+var bootstrap_validation_forms = function(form, options) {
+  $('#' + form).bootstrap_validator(options);
+};
