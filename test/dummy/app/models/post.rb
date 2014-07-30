f2ebb114f40d86ba8d7147eeeaf5275ea6739b97
Jack A. Huang <huynhquancam@gmail.com>
2014-07-30 13:21:21 +0700

    Remove unnecessary migration in dummy app

--- test/dummy/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :title, presence: true, length: {within: 20..50}, numericality: {greater_than_or_equal_to: 20, odd: true}
-  validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :intro, presence: true, if: :published?
-  validates :accepted, acceptance: true
-end
--- test/dummy/db/migrate/20140723124112_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :title
-      t.string :intro
-      t.string :accepted
-    end
-  end
-end
