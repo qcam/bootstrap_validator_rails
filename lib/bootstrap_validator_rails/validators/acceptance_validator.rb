a496f3287c65f80188a0315a40173ff9791ff202
Jack A. Huang <huynhquancam@gmail.com>
2014-07-30 16:55:23 +0700

    Update Acceptance validator #i18n_message

--- lib/bootstrap_validator_rails/validators/acceptance_validator.rb
+++ lib/bootstrap_validator_rails/validators/acceptance_validator.rb
@@ -1,8 +1,8 @@
 module BootstrapValidatorRails
   module Validators
     class Acceptance < Presence
-      def generate_message
-        'should be accepted'
+      def i18n_message
+        @record.errors.generate_message(@method, :accepted, default: '')
       end
     end
   end
--- spec/bootstrap_validator_rails/validators/acceptance_validator_spec.rb
+++ spec/bootstrap_validator_rails/validators/acceptance_validator_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 describe BootstrapValidatorRails::Validators::Acceptance do
-  let(:bootstrap_validator) { described_class.new(nil, nil, nil) }
+  class AcceptanceProduct
+    include ActiveModel::Validations
+
+    attr_accessor :terms_and_conditions
+
+    validates :terms_and_conditions, acceptance: {message: 'Accept our T&C please'}
+  end
+
+  let(:product) { AcceptanceProduct.new }
+  let(:validator) { AcceptanceProduct.validators_on(:terms_and_conditions).first }
+  let(:bootstrap_validator) { described_class.new(product, :terms_and_conditions, validator) }
 
   it 'should be a instance of Presence Validator' do
     expect(bootstrap_validator).to be_a BootstrapValidatorRails::Validators::Presence
+    html_attributes = bootstrap_validator.html_attributes
+    expect(html_attributes[:bv_notempty_message]).to eq 'Accept our T&C please'
   end
 end
