06252b631ee56c72f2ca6366e198e35140c40ff5
Jack A. Huang <huynhquancam@gmail.com>
2014-07-30 10:29:51 +0700

    Update Presence Validator spec

--- spec/bootstrap_validator_rails/validators/presence_validator_spec.rb
+++ spec/bootstrap_validator_rails/validators/presence_validator_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
+class Product
+  include ActiveModel::Validations
+
+  attr_accessor :title
+
+  validates :title, presence: true
+end
+
 describe BootstrapValidatorRails::Validators::Presence do
-  let(:post) { Post.new }
+  let(:post) { Product.new }
   let(:method) { :title }
-  let(:validator) { Post.validators_on(method) }
+  let(:validator) { Product.validators_on(method) }
   let(:bootstrap_validator) { described_class.new(post, method, validator) }
   
   describe '#generate_data' do
@@ -11,6 +19,23 @@ describe BootstrapValidatorRails::Validators::Presence do
       generated_data = bootstrap_validator.generate_data
       expect(generated_data).to have_key :bv_notempty
       expect(generated_data).to have_key :bv_notempty_message
+    end
+  end
+
+  describe '#generate_object' do
+    it 'generates object for javascript' do
+      generate_object = bootstrap_validator.generate_object
+      expect(generate_object).to eq(
+        {
+          'product[title]' => {
+            'validators' => {
+              'notEmpty' => {
+                'message' => "can't be blank"
+              }
+            }
+          } 
+        }
+      )
     end
   end
 end
